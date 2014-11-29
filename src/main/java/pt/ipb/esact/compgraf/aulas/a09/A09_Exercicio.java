36a691a81dc76cbdcd78fc38511988fd4b30a75e
Luís Lobo <ellobo@ipb.pt>
2014-11-29 10:23:35 +0000

    - exercício 9

---
+++ src/main/java/pt/ipb/esact/compgraf/aulas/a09/A09_Exercicio.java
@@ -0,0 +1,105 @@
+package pt.ipb.esact.compgraf.aulas.a09;
+
+import pt.ipb.esact.compgraf.tools.Camera;
+import pt.ipb.esact.compgraf.tools.Cameras;
+import pt.ipb.esact.compgraf.tools.DefaultGLWindow;
+
+public class A09_Exercicio extends DefaultGLWindow {
+
+	public A09_Exercicio() {
+		super("A09 Obj", true);
+		setMousePan(true);
+		setMouseZoom(true);
+	}
+	
+	@Override
+	public void init() {
+		// Definir a cor de background (RGBA={0, 0, 0, 255})
+		glClearColor(0.0f, 0.0f, 0.0f, 1f);
+		
+		// Activar o teste de profundidade
+		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_MULTISAMPLE);
+		glEnable(GL_CULL_FACE);
+
+		glCullFace(GL_BACK);
+
+		// Configurar as luzes
+		configureLighting();
+		
+		// Configurar os materiais
+		configureMaterials();
+	
+	}
+
+	private void configureMaterials() {
+		// Configurar Color Tracking
+		glEnable(GL_COLOR_MATERIAL);
+		glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+		glMateriali(GL_FRONT, GL_SHININESS, 100);
+		
+		// Especularidade do material definida explicitamente
+		glMaterialfv(GL_FRONT, GL_SPECULAR, newFloatBuffer(1.0f, 1.0f, 1.0f, 1.0f));
+	}
+
+	private void configureLighting() {
+		// Ativar a Lighting globalmente
+		glEnable(GL_LIGHTING);
+		
+		// Definição do Modelo de luz para a luz ambiente
+		glLightModelfv(GL_LIGHT_MODEL_AMBIENT, newFloatBuffer(0.1f, 0.1f, 0.1f, 1.0f));
+
+		// Este é o array com o RGB da luz ambiente
+		// Este é o array com o RGB da luz difusa
+		// Este é o array com o RGB da luz especular
+		// Configurar e Activar a Luz 0
+		glLightfv(GL_LIGHT0, GL_AMBIENT, newFloatBuffer(0.2f, 0.2f, 0.2f, 1.0f));		// Componente ambiente
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, newFloatBuffer(0.4f, 0.4f, 0.4f, 1.0f));		// Componente difusa
+		glLightfv(GL_LIGHT0, GL_SPECULAR, newFloatBuffer(0.5f, 0.5f, 0.5f, 1.0f));		// Componente especular
+
+		// Activação da luz 0
+		glEnable(GL_LIGHT0);
+	}
+
+	@Override
+	public void release() {
+		// Libertar as texturas (GPU)z
+	}
+	
+	// Variavel para a rotação do wheatley
+	private float angle = 0.0f;
+
+	void updateRotation() {
+		// Actualizar o Angulo de Rotacao
+		angle += GL_PI / 10.0f * timeElapsed();
+		angle %= 2.0f * GL_PI;
+	}
+
+	
+	@Override
+	public void render(int width, int height) {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+		// Reposicionar a luz
+		glLightfv(GL_LIGHT0, GL_POSITION, newFloatBuffer(0.0f, 3.0f, 0.0f, 1.0f));
+		
+		// Cor por omissão do cenário
+		glColor3f(0.5f, 0.5f, 0.5f);
+
+
+	}
+
+	
+	@Override
+	public void resize(int width, int height) {
+		setProjectionPerspective(width, height, 100.0f, 0.001f, 500.0f);
+		Cameras.setCurrent(new Camera(3, 3, 3));
+		setupCamera();
+	}
+
+	// Função main confere capacidade de executável ao .java atual
+	public static void main(String[] args) {
+		new A09_Exercicio();
+	}
+
+}
